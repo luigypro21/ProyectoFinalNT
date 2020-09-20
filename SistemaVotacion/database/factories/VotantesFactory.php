fb154c08f11288133961874828d19da4b310f790
luigy <luigy@DESKTOP-FDGMJJA.local>
2020-09-20 15:31:09 -0500

    modelos Votantes y Papeletas

---
+++ SistemaVotacion/database/factories/VotantesFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Votantes;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class VotantesFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Votantes::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
